dd09ee08854662fe11b2b5ea7d19b5787082b501
Nika1411 <niksel99.99@mail.ru>
2020-11-30 09:51:08 +0300

    пример

---
+++ primer.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+if __name__ == '__main__':
+    with open('text.txt', 'r') as f:
+        text = f.read()
+
+    # Заменить символы конца предложения.
+    text = text.replace("!", ".")
+    text = text.replace("?", ".")
+
+    # Удалить все многоточия.
+    while ".." in text:
+        text = text.replace("..", ".")
+
+    # Разбить текст на предложения.
+    sentences = text.split(".")
+
+    # Вывод предложений с запятыми.
+    for sentence in sentences:
+        if "," in sentence:
+            with open('text.txt', 'r')as s:
+                f_text = s.read()
+                if sentence in f_text:
+                    print(f'{sentence}{f_text[f_text.rfind(sentence) + len(sentence)]}')
