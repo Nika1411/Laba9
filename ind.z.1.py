74d7916eddf424f43e031ab63d770b44f15c67c4
Nika1411 <niksel99.99@mail.ru>
2020-12-15 01:50:33 +0300

    Индивидуальное задание 1

--- ind.z.1.py
+++ ind.z.1.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# Написать программу, которая считывает текст из файла и определяет, сколько в нем слов,
-# состоящих из не менее чем семи букв.
+#   Написать программу, которая считывает текст из файла и определяет, сколько в нем слов,
+#   состоящих из не менее чем семи букв.
 
 if __name__ == '__main__':
     with open('text.txt', 'r') as f:
         text = f.read()
-    a = []
-    for i in text.split('\n'):
-        for f in i:
-            if len(f) >= 7:
-                a += i.split()
-    print(len(set(a)))
+    text = text.replace(".", " ")
+    text = text.replace(",", " ")
+    text = text.replace(";", " ")
+    text = text.replace("!", " ")
+    text = text.replace("?", " ")
+    sentences = text.split(" ")
+
+    for i in text:
+        text.replace(i, ' ')
+    sentences = [i for i in sentences if len(i) > 6]
+
+    print(f'{sentences}')
+    print(f'Слов, состоящих из 7 букв: {len(sentences)}')
